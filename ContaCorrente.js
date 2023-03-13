ae53942b32db18a8892af03c72d5804cf800f6fe
Luana <santosmeiraluana@gmail.com>
2023-03-13 16:23:30 -0300

    unificando a conta

--- ContaCorrente.js
+++ ContaCorrente.js
@@ -26,29 +26,7 @@ export class ContaCorrente{
     constructor(agencia, cliente){
         this.agencia = agencia;
         this.cliente = cliente;
+        this._saldo = 0;
         ContaCorrente.numeroDeContas += 1;
-    }
-
-
-    sacar(valor){
-        if(this._saldo >= valor){
-            this._saldo -= valor;
-            return valor;
-        }
-    }
-
-    depositar(valor){
-        if(valor <= 0)
-        {
-            return;
-        } 
-        this._saldo += valor;           
-    }
-
-    tranferir(valor, conta){
-        
-        const valorSacado = this.sacar(valor);
-        conta.depositar(valorSacado);
-        
     }
 }
