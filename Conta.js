5fc8104717533b36817ade54f9e220ff52208a2b
Luana <santosmeiraluana@gmail.com>
2023-03-15 08:34:03 -0300

    exibindo mensagem no momento da criação de uma new Conta

--- Conta.js
+++ Conta.js
@@ -3,6 +3,9 @@ export class Conta {
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
+        if(this.constructor == Conta){
+            console.log("Você não deveria instanciar um objeto do tipo conta");
+        }
     }
 
     set cliente(novoValor) {
@@ -20,18 +23,21 @@ export class Conta {
     }
 
     sacar(valor) {
-        let taxa = 1.1;
+        let taxa = 1;
+        return this._sacar(valor, taxa);
+    }
+
+    _sacar(valor, taxa){
         const valorSacado = taxa * valor;
         if(this._saldo >= valorSacado) {
             this._saldo -= valorSacado;
             return valorSacado;
         }
+
+        return 0;
     }
 
     depositar(valor) {
-        if (valor <= 100) {
-            return;
-        }
         this._saldo += valor;
     }
 
