4fad7eb2311e59bf9efa6da6f80fc1b601b6560f
Luana <santosmeiraluana@gmail.com>
2023-03-16 07:27:22 -0300

    metodos abstratos

--- Conta.js
+++ Conta.js
@@ -1,11 +1,14 @@
+//Classe abstrata
+
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo conta diretamente, pois essa é uma classe abstrata");
+        }
+        
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-        if(this.constructor == Conta){
-            console.log("Você não deveria instanciar um objeto do tipo conta");
-        }
     }
 
     set cliente(novoValor) {
@@ -22,9 +25,9 @@ export class Conta {
         return this._saldo;
     }
 
+    //método abstrato
     sacar(valor) {
-        let taxa = 1;
-        return this._sacar(valor, taxa);
+        throw new Error("O método sacar da conta é abstrato")
     }
 
     _sacar(valor, taxa){
